dc39e438637d0c68415024b24f2f7aed6f5e4214
guilhermelkx7@gmail.com <guilhermelkx7@gmail.com>
2024-09-18 14:53:50 -0300

    atividade23

---
+++ atividade_23.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número do usuário e exiba a tabuada desse número de 1 a 10.
+numero = int(input("Digite um número para ver sua tabuada: "))
+
+print(f"Tabuada do {numero}:")
+for i in range(1, 11):
+    resultado = numero * i
+    print(f"{numero} x {i} = {resultado}")
+    
